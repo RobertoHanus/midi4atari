c87e4603530a401d2cab8ff9850dddb4ca49650e
RobertoHanus <robertohanus@hotmail.com>
2022-06-23 10:02:37 -0400

    OK

---
+++ main.asm
@@ -0,0 +1,1 @@
+      blk reloc main›      LDY #0›      LDX #0›      LDA #0›      JSR delay›      RTS››delay LDA #0›      RTS›      STA 18›      STA 19›      STA 20›delay_a CMP 20›      BNE delay_a›delay_x  CPX 19›      BNE delay_x›delay_y CMP 18›      BNE delay_y›      RTS››      blk update addresses›     end›››››
--- tests/delay.asm
+++ tests/delay.asm
@@ -1,1 +1,1 @@
-delay LDX #0›      LDY #0›delay_again_00 DEY›      BNE delay_again_00›      DEX›      BNE delay_again_00›      RTS›
+* One milisecond delay›delay LDX #0›      LDY #233›delay_again_00 DEY›      BNE delay_again_00›      DEX›      BNE delay_again_00›      RTS›
