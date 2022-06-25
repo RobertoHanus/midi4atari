23191754d860da00249159cb81f6e98f5b4ee76e
RobertoHanus <robertohanus@hotmail.com>
2022-06-24 21:32:00 -0400

    Great!

--- main.asm
+++ main.asm
@@ -44,106 +44,107 @@ syscall equ $0787 ;byte length
 ; -------------------------
 ; Main program
 ; -------------------------
-      blk reloc main
-      ; Read cl parameter
-      JSR U_GETPAR
-      BNE openfile
-      JSR PRINTF
-      dta c'Must include a MIDI file parameter!'
-      dta b($9B,$00)
-      RTS
+    blk reloc main
+    ; Read cl parameter
+    JSR U_GETPAR
+    BNE openfile
+    JSR PRINTF
+    dta c'Must include a MIDI file parameter!'
+    dta b($9B,$00)
+    RTS
 
 openfile JSR PRINTF
-      ; Prints file name on cli
-      dta c'Opening file %s'
-      dta b($9B,$00)
-      dta v(COMTAB+$21)
+    ; Prints file name on cli
+    dta c'Opening file %s'
+    dta b($9B,$00)
+    dta v(COMTAB+$21)
 
-      LDA filename
-      STA FILE_P
-      LDA filename+1
-      STA FILE_P+1
-      LDA #$04 ;File mode opening
-      STA fmode
-      LDA #$A0 ;Selected file attributes
-      STA fatr1
-      JSR FOPEN
+    LDA filename
+    STA FILE_P
+    LDA filename+1
+    STA FILE_P+1
+    LDA #$04 ;File mode opening
+    STA fmode
+    LDA #$A0 ;Selected file attributes
+    STA fatr1
+    JSR FOPEN
 
 ; Get file length
-      JSR FILELENG
+    JSR FILELENG
 ; Print file length to console
-      JSR PRINTF
-      dta c'File length: %b'
-      dta b($9B,$00)
-      dta v(faux1)
+    JSR PRINTF
+    dta c'File length: %b'
+    dta b($9B,$00)
+    dta v(faux1)
 
 ; Memory allocation
-      LDA faux1
-      STA faux4
-      LDA faux1+1
-      STA faux4+1
-      LDY #0
-      LDX #$00 ;memory index
-      JSR MALLOC
-      BPL save_memory_block
+    LDA faux1
+    STA faux4
+    LDA faux1+1
+    STA faux4+1
+    LDY #0
+    LDX #$00 ;memory index
+    JSR MALLOC
+    BPL save_memory_block
 ; Print error message
-      JSR PRINTF
-      dta c'Can''t allocate memory'
-      dta b($9B,$00)
-      RTS
+    JSR PRINTF
+    dta c'Can''t allocate memory'
+    dta b($9B,$00)
+    RTS
 
 save_memory_block LDA faux1
-      STA block_pointer
-      LDA faux1+1
-      STA block_pointer+1
-      LDA faux4
-      STA block_size
-      LDA faux4+1
-      STA block_size+1
+    STA block_pointer
+    LDA faux1+1
+    STA block_pointer+1
+    LDA faux4
+    STA block_size
+    LDA faux4+1
+    STA block_size+1
 
 ; Print allocated memory address
-      JSR PRINTF
-      dta c'Memory block address %4x'
-      dta b($9B,$00)
-      dta v(block_pointer)
+    JSR PRINTF
+    dta c'Memory block address %4x'
+    dta b($9B,$00)
+    dta v(block_pointer)
 
 ; Print allocated memory size
-      JSR PRINTF
-      dta c'Memory block size %4x'
-      dta b($9B,$00)
-      dta v(block_size)
+    JSR PRINTF
+    dta c'Memory block size %4x'
+    dta b($9B,$00)
+    dta v(block_size)
 
 ; Loads file on allocated memory
 ; Set buffer address
-      LDA block_pointer
-      STA faux1
-      LDA block_pointer+1
-      STA faux1+1
+    LDA block_pointer
+    STA faux1
+    LDA block_pointer+1
+    STA faux1+1
 ; Set buffer size
-      LDA block_size
-      STA faux4
-      LDA block_size+1
-      STA faux4+1
-      LDA #$00 ;memory index
-      STA syscall
-      JSR FREAD
-      BPL load_success
+    LDA block_size
+    STA faux4
+    LDA block_size+1
+    STA faux4+1
+    LDA #$00 ;memory index
+    STA syscall
+    JSR FREAD
+    BPL load_success
 ; Print error message
-      JSR PRINTF
-      dta c'Can''t load binary file on memory'
-      dta b($9B,$00)
-      RTS     
+    JSR PRINTF
+    dta c'Can''t load binary file on memory'
+    dta b($9B,$00)
+    RTS     
 
-load_success LDA block_pointer
-      STA $80
-      LDA block_pointer+1
-      STA $81
-      LDY #1
-      LDA ($80),Y
-      JSR PUTC
+load_success 
+    LDA block_pointer
+    STA $80
+    LDA block_pointer+1
+    STA $81
+    LDY #0
+    LDA ($80),Y
+    JSR PUTC
 
-      JSR FCLOSE
-      RTS
+    JSR FCLOSE
+    RTS
 
 ; -------------------------
 ; Subroutines
@@ -153,18 +154,18 @@ load_success LDA block_pointer
 ; X = 1/60 * 256 seconds wait
 ; Y = 1/60 * 256 * 256 seconds wait
 delay PHA
-      LDA #0
-      STA 18
-      STA 19
-      STA 20
-      PLA
+    LDA #0
+    STA 18
+    STA 19
+    STA 20
+    PLA
 delay_y CPY 18
-      BNE delay_y
+    BNE delay_y
 delay_x  CPX 19
-      BNE delay_x
+    BNE delay_x
 delay_a CMP 20
-      BNE delay_a
-      RTS
+    BNE delay_a
+    RTS
 
 ; -------------------------
 ; Variables 
@@ -179,6 +180,6 @@ filename dta v(COMTAB+$21)
 block_pointer dta a(0)
 block_size dta a(0)
 
-      blk update addresses
-      blk update symbols
-      end
+    blk update addresses
+    blk update symbols
+    end
