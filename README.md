07664088e31a1e4e66b84b5e50114e7bf201877a
RobertoHanus <robertohanus@hotmail.com>
2022-07-01 12:27:05 -0400

    Great!

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # MIDI4ATARI
 
 Application to play MIDI music files on ATARI 8Bits PC.
+
+This software was developed by Roberto Arturo Hanus Morales
+in Santiago de Chile and is protected by GNU license.
+ 
+Este software es libre para todo uso, según la norma GNU.
+Se debe respetar el nombre del autor que realizó
+esta versión original del código. De lo contrario
+un intento de apropiación indebida implicará
+en las sanciones y medidas contempladas en la 
+legislación nacional e internacional.
--- main.asm
+++ main.asm
@@ -1,3 +1,13 @@
+; This software was developed by Roberto Arturo Hanus Morales
+; in Santiago de Chile and is protected by GNU license.
+; 
+; Este software es libre para todo uso, según la norma GNU.
+; Se debe respetar el nombre del autor que realizó
+; esta versión original del código. De lo contrario
+; un intento de apropiación indebida implicará
+; en las sanciones y medidas contempladas en la 
+; legislación nacional e internacional.
+
 ; -------------------------
 ; Symbols declaration
 ; -------------------------
